887a5905f238faa7cb2c5d3d7d59c1891174b216
Cameron Seebach <cseebach@sinfoniasolutions.com>
2012-09-01 12:23:40 -0700

    initial commit: working canvas

---
+++ __main__.py
@@ -0,0 +1,35 @@
+import bottle
+from bottle.ext import sqlite
+
+__author__ = 'spirulence'
+
+app = bottle.Bottle()
+db_plugin = sqlite.Plugin(dbfile="windmaster.db")
+app.install(db_plugin)
+
+@app.route("/")
+@bottle.view("main.html")
+def game():
+    return dict()
+
+@app.route("/lib/impact/<jsfile>")
+def impact_code(jsfile):
+    return bottle.static_file(jsfile, "lib/impact/")
+
+@app.route("/lib/game/<jsfile>")
+def game_code(jsfile):
+    return bottle.static_file(jsfile, "lib/game/")
+
+@app.route("/lib/game/entities/<jsfile>")
+def game_entities(jsfile):
+    return bottle.static_file(jsfile, "lib/game/entities/")
+
+@app.route("/lib/game/levels/<jsfile>")
+def game_levels(jsfile):
+    return bottle.static_file(jsfile, "lib/game/levels/")
+
+@app.route("/media/<asset>")
+def game_assets(asset):
+    return bottle.static_file(asset, "media/")
+
+bottle.run(app, port=8081)
