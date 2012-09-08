00216a02bdd744af0ae8cdd2e217abd8fcc6793a
Cameron Seebach <cseebach@sinfoniasolutions.com>
2012-09-07 19:17:28 -0700

    separated into an __init__ file with routing information, and a __main__ file for standalone running

---
+++ __init__.py
@@ -0,0 +1,37 @@
+from gevent import monkey; monkey.patch_all()
+import bottle
+from bottle.ext import sqlite
+
+__author__ = 'spirulence'
+
+app = bottle.Bottle()
+db_plugin = sqlite.Plugin(dbfile="windmaster.db")
+app.install(db_plugin)
+
+@app.route("/")
+def game():
+    return bottle.static_file("main.html", ".")
+
+@app.route("/lib/<othercode>")
+def other_code(othercode):
+    return bottle.static_file(othercode, "lib/")
+
+@app.route("/lib/impact/<jsfile>")
+def impact_code(jsfile):
+    return bottle.static_file(jsfile, "lib/impact/")
+
+@app.route("/lib/game/<jsfile>")
+def game_code(jsfile):
+    return bottle.static_file(jsfile, "lib/game/")
+
+@app.route("/lib/game/entities/<jsfile>")
+def game_entities(jsfile):
+    return bottle.static_file(jsfile, "lib/game/entities/")
+
+@app.route("/lib/game/levels/<jsfile>")
+def game_levels(jsfile):
+    return bottle.static_file(jsfile, "lib/game/levels/")
+
+@app.route("/media/<asset>")
+def game_assets(asset):
+    return bottle.static_file(asset, "media/")
