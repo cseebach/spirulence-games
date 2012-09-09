b9750d75d90eb76ea73a755c9e3db401601cc3fe
Cameron Seebach <cseebach@sinfoniasolutions.com>
2012-09-08 17:35:34 -0700

    website reloads on module change

--- __main__.py
+++ __main__.py
@@ -3,4 +3,4 @@ import bottle
 from spirulence_games import app
 
 if __name__ == "__main__":
-    bottle.run(app, host="0.0.0.0", port=8081, server="gevent")
+    bottle.run(app, host="0.0.0.0", port=8081, server="gevent", reloader=True)
